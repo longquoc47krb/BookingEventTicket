8cbe4289c5329728901a88ccfb6912e217625b89
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2022-08-31 08:39:14 +0700

    second commit and testing

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/models/Customer.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/models/Customer.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
-@Document("Customer")
+@Document("customer")
 
 public class Customer {
     @Id
