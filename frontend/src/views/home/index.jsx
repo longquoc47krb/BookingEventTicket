8c70bf38d76aacd3654fa9faa65f3bb9e7710000
Long Quoc Nguyen <longquoc47krb@Gmail.com>
2022-09-22 08:18:38 +0700

    + Update Carousel + Update Event Component + Update Home Page

--- frontend/src/views/home/index.jsx
+++ frontend/src/views/home/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Carousel from "../../components/common/carousel";
 import Footer from "../../components/common/footer";
 import Header from "../../components/common/header";
 import SiderBar from "../../components/common/sider";
 import Typography from "../../components/common/typography";
 import HelmetHeader from "../../components/helmet";
+import { getEvents } from "../../redux/slices/eventSlice";
 function Home() {
+  const dispatch = useDispatch();
+  const events = useSelector((state) => state.event.events);
+
+  useEffect(() => {
+    dispatch(getEvents());
+  }, []);
   return (
     <>
       <HelmetHeader title="Trang chủ" content="Home page" />
@@ -13,7 +22,7 @@ function Home() {
       <div className="home-container">
         <SiderBar className="sider" />
         <div className="home-content">
-          <Carousel />
+          <Carousel data={events} />
           <div className="home-popular">
             <Typography className="text-center">Sự kiện nổi bật</Typography>
           </div>
