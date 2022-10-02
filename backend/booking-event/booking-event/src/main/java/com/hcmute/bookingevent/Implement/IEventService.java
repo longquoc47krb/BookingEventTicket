89c569a83a57a774101031bdc5b9b22149c3bc4a
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2022-10-03 00:13:39 +0700

    add pagination in event and account

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/Implement/IEventService.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/Implement/IEventService.java
@@ -2,6 +2,7 @@ package com.hcmute.bookingevent.Implement;
 
 import com.hcmute.bookingevent.models.Event;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,8 @@ public interface IEventService {
     ResponseEntity<?> createEvent(Event event);
     ResponseEntity<?> findAllEvents();
     ResponseEntity<?> deleteEvent( String id);
-
     ResponseEntity<?> findEventById(String id);
     ResponseEntity<?> searchEvents(String key);
     ResponseEntity<?> findEventListById(String id);
+    ResponseEntity<?> eventPagination(Pageable pageable);
 }
--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/controllers/AccountController.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/controllers/AccountController.java
@@ -6,6 +6,10 @@ import com.hcmute.bookingevent.models.Account;
 import com.hcmute.bookingevent.Implement.IAccountService;
 import lombok.AllArgsConstructor;
 
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +23,19 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(path = "/account")
 public class AccountController {
     private  final  IAccountService iAccountService;
+
+    @GetMapping(path = "/admin/manage/users")
+    public ResponseEntity<?> findAll (@RequestParam(value = "currentPage", defaultValue = "0") int currentPage,@RequestParam(value="pageSize", defaultValue = "5") int pageSize   ){
+        Pageable pageable = PageRequest.of(currentPage, pageSize);
+        return iAccountService.findAll(pageable);
+    }
     @GetMapping("/findAll")
     public ResponseEntity<?> findAll() {
         return iAccountService.findAll();
-
     }
     @GetMapping("/findAccount")
     public ResponseEntity<?> findAccountByPhoneOrNameOrGmail(@RequestParam(value="value")  String value) {
         return iAccountService.findByPhoneOrNameOrGmail(value);
-
     }
     @PostMapping("/loginByPhone")
     public ResponseEntity<?> loginAccountByPhone(@RequestBody Account newAccount) {
