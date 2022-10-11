3b6c1f911ea2196ea39e46463b5eb3d068c8a719
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2022-10-11 21:50:52 +0700

    set up Swagger successfully

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/controllers/AuthController.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/controllers/AuthController.java
@@ -4,10 +4,7 @@ import com.hcmute.bookingevent.services.MailService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 
@@ -20,7 +17,7 @@ public class AuthController {
 
 
 //    @ResponseBody
-    @RequestMapping("/sendHtmlEmail")
+    @RequestMapping(value = "/sendHtmlEmail",method = RequestMethod.POST)
     public ResponseEntity<?> sendHtmlEmail(@RequestParam(value="name")  String name) throws MessagingException {
         return mailService.sendMail(name);
 
