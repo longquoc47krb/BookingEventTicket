a34813658fa3d10187a98f313f18d0e8e2c4c3fb
Long Quoc Nguyen <longquoc47krb@gmail.com>
2022-11-10 15:26:57 +0700

    Change avatar default

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/common/Constants.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/common/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_ORGANIZATION = "ROLE_ORGANIZATION";
     //public static final String METHOD_GOOGLE = "METHOD_GOOGLE";
-    public static final String AVATAR_DEFAULT="https://microbiology.ucr.edu/sites/default/files/styles/form_preview/public/blank-profile-pic.png?itok=4teBBoet&fbclid=IwAR03Tfvnn76qi4wfpBrxw004mEad1Ho9qR89fF_D4jyBAwaE5DdXWa4ltjU";
+    public static final String AVATAR_DEFAULT="shorturl.at/qrxMO";
 
     public static String getAlphaNumericString(int n) {
 
