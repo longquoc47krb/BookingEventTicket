034ff64dd03b189682ad2e56ee4eb5bd8ceea78c
sahb9 <swan4567890@gmail.com>
2022-11-28 00:24:20 +0700

    add checkOrderAvailability in OrderController.java

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/services/OrderService.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/services/OrderService.java
@@ -24,6 +24,7 @@ import static com.hcmute.bookingevent.services.TicketService.setStatusForTicketT
 
 @Service
 @RequiredArgsConstructor
+//@Transactional
 public class OrderService implements IOrderService {
     private final OrderRepository orderRepository;
     private final CustomerRepository customerRepository;
@@ -31,16 +32,15 @@ public class OrderService implements IOrderService {
 
     private final EventRepository eventRepository;
 
+    //handle order after meeting all secure criteria
     @Override
     public ResponseEntity<?> createCustomerOrder(String email, Order order) {
-        Optional<Customer> customer = customerRepository.findByEmail(email);
-        if (customer.isPresent()) {
-            // cap nhat o event
+        try {
+            Optional<Customer> customer = customerRepository.findByEmail(email);
             Optional<Event> event = eventRepository.findEventById(order.getIdEvent());
-            if (event.isPresent()) {
-                // cap nhat o event
+            if (customer.isPresent() && event.isPresent()) {
                 event.get().setTicketRemaining(event.get().getTicketRemaining() - order.getTotalQuantity());
-
+                //
                 for (Ticket ticketOfCustomer : order.getCustomerTicketList()) {
                     for (Ticket ticket : event.get().getOrganizationTickets()) {
                         if (ticket.getId().equals(ticketOfCustomer.getId())) {
@@ -49,41 +49,60 @@ public class OrderService implements IOrderService {
                         }
                     }
                 }
+                //cap nhat o organization  (cong tien)
+                Optional<Organization> organization = organizationRepository.findOrganizationByEventId(order.getIdEvent());
+                if (organization.isPresent()) {
+                    BigDecimal orderPrice = new BigDecimal(order.getTotalPrice());
+
+                    if (order.getCurrency().equals("USD")) {
+                        BigDecimal usdBalance = new BigDecimal(organization.get().getUSDBalance());
+                        organization.get().setUSDBalance((usdBalance.add(orderPrice)).toString());
+                    } else if (order.getCurrency().equals("VND")) {
+                        BigDecimal vndBalance = new BigDecimal(organization.get().getVNDBalance());
+                        organization.get().setVNDBalance(vndBalance.add(orderPrice).toString());
+                    } else {
+                        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                                new ResponseObject(false, "Problem with currency ", "", 400));
+
+                    }
+                    organizationRepository.save(organization.get());
+                }
                 eventRepository.save(event.get());
+                orderRepository.save(order);
+                customerRepository.save(customer.get());
+
+                return ResponseEntity.status(HttpStatus.OK).body(
+                        new ResponseObject(true, "Create Order for Customer successfully ", orderRepository.findById(order.getId()), 200));
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                        new ResponseObject(false, "Create Order fail with email:" + email, "", 404));
             }
-            //cap nhat o organization  (cong tien)
-            Optional<Organization> organization =  organizationRepository.findOrganizationByEventId(order.getIdEvent());
-            if(organization.isPresent())
-            {
-                BigDecimal orderPrice = new BigDecimal(order.getTotalPrice());
-
-                if(order.getCurrency().equals("USD"))
-                {
-                    BigDecimal usdBalance = new BigDecimal(organization.get().getUSDBalance());
-                    organization.get().setUSDBalance(usdBalance.add(orderPrice).toString());
-                }
-                else if(order.getCurrency().equals("VND"))
-                {
-                    BigDecimal vndBalance = new BigDecimal(organization.get().getVNDBalance());
-                    organization.get().setVNDBalance(vndBalance.add(orderPrice).toString());
-                }
-                else
-                {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                            new ResponseObject(false, "Problem with currency ", "", 400));
-
-                }
-                organizationRepository.save(organization.get());
-            }
-            orderRepository.save(order);
-            customerRepository.save(customer.get());
-
-            return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject(true, "Create Order for Customer successfully ", orderRepository.findById(order.getId()), 200));
-        } else {
+            //throw new Exception("test");
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject(false, "Create Order fail with email:" + email, "", 404));
+                    new ResponseObject(false, "Create Order fail with email:" + email, e.getMessage(), 404));
         }
+    }
+    @Override
+    public ResponseEntity<?> checkOrderAvailability( Order order) {
+        Optional<Event> event = eventRepository.findEventById(order.getIdEvent());
+        if(event.isPresent())
+        {
+            for (Ticket ticketOfCustomer : order.getCustomerTicketList()) {
+                for (Ticket ticket : event.get().getOrganizationTickets()) {
+                    //
+                    if (ticket.getId().equals(ticketOfCustomer.getId())) {
+                        if(ticket.getQuantityRemaining() - ticketOfCustomer.getQuantity() + 1<0)
+                        {
+                            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(
+                                    new ResponseObject(false, "check  Order availability fail with ticket: " + ticket.getTicketName() ,"", 400));
+                        }
+                    }
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(true, "check  Order availability successfully" , "", 200));
     }
 
     @Override
@@ -100,52 +119,44 @@ public class OrderService implements IOrderService {
 
         }
     }
+
     @Override
     public ResponseEntity<?> findAll() {
 
-        List<Order> orderList= orderRepository.findAll();
-        if(orderList.size()>0)
-        {
+        List<Order> orderList = orderRepository.findAll();
+        if (orderList.size() > 0) {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject(true, "find All order" , orderList, 200));
+                    new ResponseObject(true, "find All order", orderList, 200));
 
-        }
-        else
-        {
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject(false, "find All order fail" ,"" , 400));
+                    new ResponseObject(false, "find All order fail", "", 400));
 
         }
 
     }
+
     @Override
-    public ResponseEntity<?> findOrderByEventId(String eventId)
-    {
-        try
-        {
+    public ResponseEntity<?> findOrderByEventId(String eventId) {
+        try {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject(true, "findOrderByEventId successfully ", orderRepository.findAllByIdEvent(eventId),200));
-        }
-        catch (Exception e)
-        {
+                    new ResponseObject(true, "findOrderByEventId successfully ", orderRepository.findAllByIdEvent(eventId), 200));
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject(false, "findOrderByEventId fail ", e.getMessage(),400));
+                    new ResponseObject(false, "findOrderByEventId fail ", e.getMessage(), 400));
 
         }
 
     }
+
     @Override
-    public ResponseEntity<?> findOrderByTicketType(String ticketTypeId)
-    {
-        try
-        {
+    public ResponseEntity<?> findOrderByTicketType(String ticketTypeId) {
+        try {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject(true, "findOrderByTicketType successfully ", orderRepository.findAllByCustomerTicketList_Id(ticketTypeId),200));
-        }
-        catch (Exception e)
-        {
+                    new ResponseObject(true, "findOrderByTicketType successfully ", orderRepository.findAllByCustomerTicketList_Id(ticketTypeId), 200));
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject(false, "findOrderByTicketType fail ", e.getMessage(),400));
+                    new ResponseObject(false, "findOrderByTicketType fail ", e.getMessage(), 400));
 
         }
 
