19c875599f02dcafcc63f79986d9f96669ee75a3
anhtuan <tuantuan3455@gmail.com>
2023-03-10 23:28:00 +0700

    update review

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/repository/ReviewRepository.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/repository/ReviewRepository.java
@@ -1,12 +1,16 @@
 package com.hcmute.bookingevent.repository;
 
+import com.hcmute.bookingevent.models.Order;
 import com.hcmute.bookingevent.models.Review;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends MongoRepository<Review,String> {
     Boolean existsByEmail(String email);
     Boolean existsByEmailAndIdEvent(String email,String idEvent);
     List<Review> findAllByIdEvent(String idEvent);
+    Optional<Review> findByEmailAndIdEvent(String email,String idEvent);
     void deleteByEmailAndIdEvent(String email, String idEvent);
 }
