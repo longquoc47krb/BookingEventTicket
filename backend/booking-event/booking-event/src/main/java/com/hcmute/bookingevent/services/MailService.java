54fab7566717a77c5799ec395d625548a1754116
anhtuan <tuantuan3455@gmail.com>
2023-05-01 23:17:14 +0700

    fix sending mail

--- backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/services/MailService.java
+++ backend/booking-event/booking-event/src/main/java/com/hcmute/bookingevent/services/MailService.java
@@ -29,19 +29,19 @@ public class MailService {
     private final JavaMailSender emailSender;
     private final Configuration configuration;
 
-    final String MAIL_TEMPLATE = "mail-template.ftl";
+    final String MAIL_TEMPLATE = "newmail-template.ftl";
 
-    final String OTP_CONTENT ="Please enter this code to verify your action with LotusTicket. This code use only once. Please do not share this code to with anyone else due to security for yourself. The code is only valid for 5 minutes. " +
-            "<br>Regards,\n";
-    final String NEWPASSWORD_CONTENT ="You are receiving this email because we received a password reset request for your account. Please visit our website to change your new password. You should keep a secure record of your password and not disclose it to any unauthorized party." +
-            "<br>Regards,";
-    final String REGISTER_CONTENT ="Welcome to LotusTicket! You have successfully registered. Please visit our website to explore " +
-            "<br>Regards,";
-    final String BECOME_ORGANIZATION_CONTENT ="Welcome to LotusTicket! You have successfully sent your application form . Please wait for a few days to allow us to verify your information before you have full authentication to our website. We will notify you  of result as soon as possible" +
-            "<br>Regards,";
+    final String OTP_CONTENT ="Please enter this code to verify your action with LotusTicket. This code use only once. Please do not share this code to with anyone else due to security for yourself. The code is only valid for 5 minutes. " ;
+            //"<br>Regards,\n";
+    final String NEWPASSWORD_CONTENT ="You are receiving this email because we received a password reset request for your account. Please visit our website to change your new password. You should keep a secure record of your password and not disclose it to any unauthorized party." ;
+            //"<br>Regards,";
+    final String REGISTER_CONTENT ="Welcome to LotusTicket! You have successfully registered. Please visit our website to explore " ;
+           // "<br>Regards,";
+    final String BECOME_ORGANIZATION_CONTENT ="Welcome to LotusTicket! You have successfully sent your application form . Please wait for a few days to allow us to verify your information before you have full authentication to our website. We will notify you  of result as soon as possible" ;
+           // "<br>Regards,";
     final String OFFICIAL_ORGANIZATION_CONTENT ="We have reviewed your application form. After taking into consideration based on our term conditions. We officially inform that you have full authorization of organization role." +
-            "<br>Please use this password below to sign in your account in our system. Click <a href=\"https://lotusticket-admin.netlify.app\">here</a> to login " +
-            "<br>Regards,";
+            "<br>Please use this password below to sign in your account in our system. Click <a href=\"https://lotusticket-admin.netlify.app\">here</a> to login " ;
+        //    "<br>Regards,";
     final String REFUSE_ORGANIZATION_CONTENT ="After reviewing your application form thoroughly, We have to refuse your form. This is because you may not satisfy one of our strict criteria" +
             "<br>Regards,";
 //    final String NEW_EVENT ="After reviewing your application form thoroughly, We have to refuse your form. This is because you may not satisfy one of our strict criteria" +
@@ -65,11 +65,14 @@ public class MailService {
             {
                 model.put("header", "Verify your account");
                 model.put("content", OTP_CONTENT);
+                model.put("contentColor", messageContent);
+
             }
             else if(type.equals(EMailType.NEW_PASSWORD))
             {
                 model.put("header", "Receive new password");
                 model.put("content", NEWPASSWORD_CONTENT);
+                model.put("contentColor", messageContent);
             }
             else if (type.equals(EMailType.REGISTER))
             {
@@ -85,21 +88,26 @@ public class MailService {
             {
                 model.put("header", "Register for organization successfully");
                 model.put("content", OFFICIAL_ORGANIZATION_CONTENT);
+                model.put("contentColor", messageContent);
+
             }
             else if(type.equals(EMailType.REFUSE_ORGANIZATION))
             {
                 model.put("header", "Refuse for organization successfully");
                 model.put("content", REFUSE_ORGANIZATION_CONTENT);
+                model.put("contentColor", messageContent);
             }
             else if(type.equals(EMailType.NEW_EVENT))
             {
                 model.put("header", "Upcoming event");
                 model.put("content", content);
             }
-
             model.put("title", "LOTUS TICKET");
             model.put("name", account.getName());
-            model.put("contentColor", messageContent);
+            if(messageContent.equals(""))
+            {
+                model.put("contentColor", "<br>");
+            }
 
             String html = FreeMarkerTemplateUtils.processTemplateIntoString(Objects.requireNonNull(template),model);
             mimeMailMessage.setContent(html, TYPE_EMAIL);
@@ -118,11 +126,9 @@ public class MailService {
             log.info("Error : {}",ex.toString());
 
         }
-
-
     }
     public void sendMailByAccountList(List<Account> accountList, String eventName, String organizationName , EMailType type) throws MessagingException, TemplateException, IOException {
-        String NEW_EVENT ="We notify you that "+organizationName +"  just created new event is "  +eventName +
+        String NEW_EVENT ="We notify you that "+ organizationName +"  just created new event is "  +eventName +
                 "<br>Regards,";
         for(Account account : accountList)
         {
